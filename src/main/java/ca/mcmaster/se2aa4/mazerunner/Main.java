f6a44205ed53fb1b3b81d846b4539dea008011ad
Raymond Tao <taor10@mcmaster.ca>
2024-01-25 19:33:18 -0500

    -method and -p structure added to main, resolve #11, resolve #12.

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -20,9 +20,18 @@ public class Main {
         Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.ALL);
 
         Options options = new Options();
+
         Option input = new Option("i", "input", true, "input file path");
         input.setRequired(true);
         options.addOption(input);
+        
+        Option pathOption = new Option("p", "path", true, "Path to validate");
+        pathOption.setRequired(false);
+        options.addOption(pathOption);
+
+        Option methodOption = new Option("m", "method", true, "Algorithm for path finding (tremaux, righthand)")
+        pathOption.setRequired(false);
+        options.addOption(methodOption);
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd;
@@ -31,6 +40,11 @@ public class Main {
         try {
             cmd = parser.parse(options, args);
 
+            String mazeMethod = "righthand"; // Default method
+            if (cmd.hasOption("method")) {
+                mazeMethod = cmd.getOptionValue("method");
+            }
+
             if (cmd.hasOption("i")) {
                 String inputFilePath = cmd.getOptionValue("i");
                 logger.info("**** Reading the maze from file " + inputFilePath);
@@ -38,28 +52,40 @@ public class Main {
                 MazeReader mazeReader = new MazeReader(inputFilePath);
                 char[][] maze = mazeReader.getMaze();
 
-                // Logging MazeReader
-                logger.info("**** Testing MazeReader.java ****");
-                for (char[] row : maze) {
-                    for (char cell : row) {
-                        logger.info(cell == '0' ? "PASS " : "WALL ");
-                    }
-                    logger.info(System.lineSeparator());
-                }
-                logger.info("**** Testing Ended of MazeRunner.java ****");
+                // // Logging MazeReader
+                // logger.info("**** Testing MazeReader.java ****");
+                // for (char[] row : maze) {
+                //     for (char cell : row) {
+                //         logger.info(cell == '0' ? "PASS " : "WALL ");
+                //     }
+                //     logger.info(System.lineSeparator());
+                // }
+                // logger.info("**** Testing Ended of MazeRunner.java ****");
 
-                BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    for (int idx = 0; idx < line.length(); idx++) {
-                        if (line.charAt(idx) == '#') {
-                            logger.trace("WALL ");
-                        } else if (line.charAt(idx) == ' ') {
-                            logger.trace("PASS ");
-                        }
+                if (cmd.hasOption("p")) {
+                    // Path validation logic
+                } else {
+                    // Path finding logic
+                    if (mazeMethod.equals("tremaux")) {
+                        // Tremaux logic
+                    } else {
+                        // Righthand logic
                     }
-                    logger.trace(System.lineSeparator());
                 }
+
+                // // OG Logger
+                // BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+                // String line;
+                // while ((line = reader.readLine()) != null) {
+                //     for (int idx = 0; idx < line.length(); idx++) {
+                //         if (line.charAt(idx) == '#') {
+                //             logger.trace("WALL ");
+                //         } else if (line.charAt(idx) == ' ') {
+                //             logger.trace("PASS ");
+                //         }
+                //     }
+                //     logger.trace(System.lineSeparator());
+                // }
             }
         } catch (Exception e) {
             logger.error("/!\\ An error has occured /!\\");
